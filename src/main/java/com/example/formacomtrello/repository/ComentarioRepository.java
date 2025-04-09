7f2d307fe4f25453b63d640cea1ed4bd8b3f8488
JEVizcaya <jevizcaya@hotmail.com>
2025-04-09 18:01:39 +0200

    todo funcionando

--- src/main/java/com/example/formacomtrello/repository/ComentarioRepository.java
+++ src/main/java/com/example/formacomtrello/repository/ComentarioRepository.java
@@ -1,7 +1,11 @@
 package com.example.formacomtrello.repository;
 
 import com.example.formacomtrello.model.Comentario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +16,9 @@ public interface ComentarioRepository extends JpaRepository<Comentario, Integer>
     List<Comentario> findByTareaId(Integer tareaId);
 
     List<Comentario> findByTareaIdOrderByFechaCreacionDesc(Integer tareaId);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Comentario c WHERE c.tarea.id = :tareaId")
+    void deleteByTareaId(@Param("tareaId") Integer tareaId);
 
 }
